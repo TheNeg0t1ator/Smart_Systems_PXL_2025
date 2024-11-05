b18111bc4befd1ce3ad2f8fa61fc6a5ed59c469c
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2024-11-05 16:07:33 +0100

    another test

--- RTSP/RTSP_IN_OUT_Complex/main.cpp
+++ RTSP/RTSP_IN_OUT_Complex/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     GstRTSPMountPoints *mounts;
     GstRTSPMediaFactory *factory;
     GMainLoop *loop;
-    GstElement *pipeline, *rtspsrc, *rtph264depay, *h264parse, *nvv4l2decoder, *nvvideoconvert, *capsfilter, *nvv4l2h264enc, *rtph264pay;
+    GstElement *bin, *rtspsrc, *rtph264depay, *h264parse, *nvv4l2decoder, *nvvideoconvert, *capsfilter, *nvv4l2h264enc, *rtph264pay;
 
     gst_init(&argc, &argv);
 
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
 
     // Create the RTSP server
     server = gst_rtsp_server_new();
-    gst_rtsp_server_set_service(server, "8554");  // Set the port to 8554
+    gst_rtsp_server_set_service(server, "8554");
 
     // Get the mount points for the server
     mounts = gst_rtsp_server_get_mount_points(server);
@@ -23,37 +23,33 @@ int main(int argc, char *argv[]) {
     // Create a media factory
     factory = gst_rtsp_media_factory_new();
 
-    // Create the pipeline and elements
-    pipeline = gst_pipeline_new("rtsp-pipeline");
+    // Create a bin for the pipeline elements
+    bin = gst_bin_new("rtsp-bin");
 
-    // Create elements for the pipeline
+    // Create elements and set properties
     rtspsrc = gst_element_factory_make("rtspsrc", "source");
+    g_object_set(rtspsrc, "location", "rtsp://EdgeCam:EdgeCam@192.168.2.11:554/stream1", NULL);
+
     rtph264depay = gst_element_factory_make("rtph264depay", "depay");
     h264parse = gst_element_factory_make("h264parse", "parser");
     nvv4l2decoder = gst_element_factory_make("nvv4l2decoder", "decoder");
     nvvideoconvert = gst_element_factory_make("nvvideoconvert", "converter");
+    
     capsfilter = gst_element_factory_make("capsfilter", "capsfilter");
+    g_object_set(capsfilter, "caps", gst_caps_from_string("video/x-raw(memory:NVMM), width=256, height=256"), NULL);
+
     nvv4l2h264enc = gst_element_factory_make("nvv4l2h264enc", "encoder");
     rtph264pay = gst_element_factory_make("rtph264pay", "pay");
-
-    if (!pipeline || !rtspsrc || !rtph264depay || !h264parse || !nvv4l2decoder || !nvvideoconvert || !capsfilter || !nvv4l2h264enc || !rtph264pay) {
-        g_printerr("Failed to create elements\n");
-        return -1;
-    }
-
-    // Set properties for elements
-    g_object_set(rtspsrc, "location", "rtsp://EdgeCam:EdgeCam@192.168.2.11:554/stream1", NULL);
-    g_object_set(capsfilter, "caps", gst_caps_from_string("video/x-raw(memory:NVMM), width=256, height=256"), NULL);
     g_object_set(rtph264pay, "pt", 96, NULL);
 
-    // Add elements to pipeline
-    gst_bin_add_many(GST_BIN(pipeline), rtspsrc, rtph264depay, h264parse, nvv4l2decoder, nvvideoconvert, capsfilter, nvv4l2h264enc, rtph264pay, NULL);
+    // Add elements to the bin
+    gst_bin_add_many(GST_BIN(bin), rtspsrc, rtph264depay, h264parse, nvv4l2decoder, nvvideoconvert, capsfilter, nvv4l2h264enc, rtph264pay, NULL);
 
-    // Link elements
+    // Link elements in the bin
     gst_element_link_many(rtspsrc, rtph264depay, h264parse, nvv4l2decoder, nvvideoconvert, capsfilter, nvv4l2h264enc, rtph264pay, NULL);
 
-    // Set the pipeline as the launch property of the factory
-    gst_rtsp_media_factory_set_element(factory, pipeline);
+    // Set the bin as the factory's element
+    gst_rtsp_media_factory_set_element(factory, bin);
 
     // Attach the media factory to the /stream mount point
     gst_rtsp_mount_points_add_factory(mounts, "/stream", factory);
