f6ff3cbbdd0ba64c16063fc58a3df603fdde4017
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2024-11-05 15:59:35 +0100

    Update main.cpp

--- RTSP/RTSP_IN_OUT_Complex/main.cpp
+++ RTSP/RTSP_IN_OUT_Complex/main.cpp
@@ -1,146 +1,75 @@
 #include <gst/gst.h>
-#include <glib.h>
-#include <stdio.h>
-#include <cuda_runtime_api.h>
-#include "gstnvdsmeta.h"
+#include <gst/rtsp-server/rtsp-server.h>
 
 int main(int argc, char *argv[]) {
-    GstElement *pipeline, *source, *depay, *pay,*sink;
-    GstBus *bus;
-    GstMessage *msg;
-    GstStateChangeReturn ret;
+    GstRTSPServer *server;
+    GstRTSPMountPoints *mounts;
+    GstRTSPMediaFactory *factory;
+    GMainLoop *loop;
+    GstElement *pipeline, *rtspsrc, *rtph264depay, *h264parse, *nvv4l2decoder, *nvvideoconvert, *capsfilter, *nvv4l2h264enc, *rtph264pay;
 
-    // Initialize GStreamer
     gst_init(&argc, &argv);
 
-    // Create the elements
-    source = gst_element_factory_make("rtspsrc", "source");
-    if (!source) {
-        g_printerr("Failed to create 'rtspsrc' element.\n");
+    // Create the main event loop
+    loop = g_main_loop_new(NULL, FALSE);
+
+    // Create the RTSP server
+    server = gst_rtsp_server_new();
+    gst_rtsp_server_set_service(server, "8554");  // Set the port to 8554
+
+    // Get the mount points for the server
+    mounts = gst_rtsp_server_get_mount_points(server);
+
+    // Create a media factory
+    factory = gst_rtsp_media_factory_new();
+
+    // Create the pipeline and elements
+    pipeline = gst_pipeline_new("rtsp-pipeline");
+
+    // Create elements for the pipeline
+    rtspsrc = gst_element_factory_make("rtspsrc", "source");
+    rtph264depay = gst_element_factory_make("rtph264depay", "depay");
+    h264parse = gst_element_factory_make("h264parse", "parser");
+    nvv4l2decoder = gst_element_factory_make("nvv4l2decoder", "decoder");
+    nvvideoconvert = gst_element_factory_make("nvvideoconvert", "converter");
+    capsfilter = gst_element_factory_make("capsfilter", "capsfilter");
+    nvv4l2h264enc = gst_element_factory_make("nvv4l2h264enc", "encoder");
+    rtph264pay = gst_element_factory_make("rtph264pay", "pay");
+
+    if (!pipeline || !rtspsrc || !rtph264depay || !h264parse || !nvv4l2decoder || !nvvideoconvert || !capsfilter || !nvv4l2h264enc || !rtph264pay) {
+        g_printerr("Failed to create elements\n");
         return -1;
     }
 
-    depay = gst_element_factory_make("rtph264depay", "depay");
-    if (!depay) {
-        g_printerr("Failed to create 'rtph264depay' element.\n");
-        return -1;
-    }
+    // Set properties for elements
+    g_object_set(rtspsrc, "location", "rtsp://EdgeCam:EdgeCam@192.168.2.11:554/stream1", NULL);
+    g_object_set(capsfilter, "caps", gst_caps_from_string("video/x-raw(memory:NVMM), width=256, height=256"), NULL);
+    g_object_set(rtph264pay, "pt", 96, NULL);
 
-    pay = gst_element_factory_make("rtph264pay", "pay");
-    if (!pay) {
-        g_printerr("Failed to create 'rtph264depay' element.\n");
-        return -1;
-    }
+    // Add elements to pipeline
+    gst_bin_add_many(GST_BIN(pipeline), rtspsrc, rtph264depay, h264parse, nvv4l2decoder, nvvideoconvert, capsfilter, nvv4l2h264enc, rtph264pay, NULL);
 
+    // Link elements
+    gst_element_link_many(rtspsrc, rtph264depay, h264parse, nvv4l2decoder, nvvideoconvert, capsfilter, nvv4l2h264enc, rtph264pay, NULL);
 
-    sink = gst_element_factory_make("udpsink", "sink");
-    if (!sink) {
-        g_printerr("Failed to create 'udpsink' element.\n");
-        return -1;
-    }
+    // Set the pipeline as the launch property of the factory
+    gst_rtsp_media_factory_set_element(factory, pipeline);
 
-    // Create the empty pipeline
-    pipeline = gst_pipeline_new("test-pipeline");
-    if (!pipeline) {
-        g_printerr("Failed to create 'pipeline' element.\n");
-        return -1;
-    }
+    // Attach the media factory to the /stream mount point
+    gst_rtsp_mount_points_add_factory(mounts, "/stream", factory);
 
-    // Scource
-    g_object_set(G_OBJECT(source), "location", "rtsp://192.168.2.219/live", NULL);
+    // Start the server
+    g_object_unref(mounts);
+    gst_rtsp_server_attach(server, NULL);
 
-    // Sink
-    g_object_set(G_OBJECT(sink), "host", "127.0.0.1", NULL);
-    g_object_set(G_OBJECT(sink), "port", 554, NULL);
-    g_object_set(G_OBJECT(sink), "auto-multicast", TRUE, NULL);
-    g_object_set(G_OBJECT(sink), "sync", TRUE, NULL);
-    g_object_set(G_OBJECT(sink), "async", FALSE, NULL);
+    g_print("RTSP server is running at rtsp://192.168.2.52:8554/stream\n");
 
+    // Run the loop
+    g_main_loop_run(loop);
 
-
-    // Build the pipeline
-    gst_bin_add_many(GST_BIN(pipeline), source, depay, sink, NULL);
-    if (!gst_element_link(depay, sink)) {
-        g_printerr("Failed to link 'depay' and 'sink' elements.\n");
-        gst_object_unref(pipeline);
-        return -1;
-    }
-
-    // Connect to the pad-added signal for the source element
-    g_signal_connect(source, "pad-added", G_CALLBACK(+[](GstElement *src, GstPad *new_pad, GstElement *depay) {
-        GstPad *sink_pad = gst_element_get_static_pad(depay, "sink");
-        GstPadLinkReturn ret;
-        GstCaps *new_pad_caps = NULL;
-        GstStructure *new_pad_struct = NULL;
-        const gchar *new_pad_type = NULL;
-
-        g_print("Received new pad '%s' from '%s':\n", GST_PAD_NAME(new_pad), GST_ELEMENT_NAME(src));
-
-        // Check the new pad's type
-        new_pad_caps = gst_pad_get_current_caps(new_pad);
-        new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
-        new_pad_type = gst_structure_get_name(new_pad_struct);
-
-        if (!g_str_has_prefix(new_pad_type, "application/x-rtp")) {
-            g_printerr("It has type '%s' which is not 'application/x-rtp'. Ignoring.\n", new_pad_type);
-            gst_caps_unref(new_pad_caps);
-            gst_object_unref(sink_pad);
-            return;
-        }
-
-        // Attempt to link the pads
-        ret = gst_pad_link(new_pad, sink_pad);
-        if (GST_PAD_LINK_FAILED(ret)) {
-            g_printerr("Type is '%s' but link failed.\n", gst_caps_to_string(new_pad_caps));
-        } else {
-            g_print("Link succeeded (type '%s').\n", gst_caps_to_string(new_pad_caps));
-        }
-
-        // Unreference the caps and pads
-        gst_caps_unref(new_pad_caps);
-        gst_object_unref(sink_pad);
-    }), depay);
-
-    // Start playing the pipeline
-    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    if (ret == GST_STATE_CHANGE_FAILURE) {
-        g_printerr("Unable to set the pipeline to the playing state.\n");
-        gst_object_unref(pipeline);
-        return -1;
-    }
-
-    // Wait until error or EOS (End of Stream)
-    bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
-
-    // Parse message
-    if (msg != NULL) {
-        GError *err;
-        gchar *debug_info;
-
-        switch (GST_MESSAGE_TYPE(msg)) {
-            case GST_MESSAGE_ERROR:
-                gst_message_parse_error(msg, &err, &debug_info);
-                g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
-                g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
-                g_clear_error(&err);
-                g_free(debug_info);
-                break;
-            case GST_MESSAGE_EOS:
-                g_print("End-Of-Stream reached.\n");
-                break;
-            default:
-                // We should not reach here because we only asked for ERRORs and EOS
-                g_printerr("Unexpected message received.\n");
-                break;
-        }
-        gst_message_unref(msg);
-    }
-
-    // Free resources
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
+    // Cleanup
+    g_object_unref(server);
+    g_main_loop_unref(loop);
 
     return 0;
 }
