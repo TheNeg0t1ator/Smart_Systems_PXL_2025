f81f8419c763792aecf40d6edce03f1f089cc024
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2024-11-07 14:59:39 +0100

    L python works

--- RTSP/RTSP_IN_OUT_Python/main.py
+++ RTSP/RTSP_IN_OUT_Python/main.py
@@ -1,88 +1,311 @@
+#!/usr/bin/env python3
+
+################################################################################
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+import sys
+sys.path.append("/home/pxl/my_apps")
+import pyds
+from common.bus_call import bus_call
+from common.is_aarch_64 import is_aarch64
+import platform
+import math
+import time
+from ctypes import *
 import gi
-gi.require_version('Gst', '1.0')
-gi.require_version('GstRtspServer', '1.0')
-from gi.repository import Gst, GstRtspServer, GObject
+gi.require_version("Gst", "1.0")
+gi.require_version("GstRtspServer", "1.0")
+from gi.repository import GObject, Gst, GstRtspServer, GLib
+import configparser
 
-# Initialize GStreamer
-Gst.init(None)
+import argparse
 
-class RTSPRelayServer(GstRtspServer.RTSPMediaFactory):
-    def __init__(self, input_uri):
-        super(RTSPRelayServer, self).__init__()
-        self.input_uri = input_uri
-        self.set_shared(True)
+from common.FPS import GETFPS
 
-    def do_create_element(self, url):
-        # Create each GStreamer element individually
-        source = Gst.ElementFactory.make("rtspsrc", "source")
-        depay = Gst.ElementFactory.make("rtph264depay", "depay")
-        decoder = Gst.ElementFactory.make("avdec_h264", "decoder")
-        videobalance = Gst.ElementFactory.make("videobalance", "videobalance")
-        encoder = Gst.ElementFactory.make("x264enc", "encoder")
-        pay = Gst.ElementFactory.make("rtph264pay", "pay")
+fps_streams = {}
 
-        # Set properties for each element
-        source.set_property("location", self.input_uri)
-        source.set_property("latency", 200)
-        videobalance.set_property("saturation", 0.0)
-        encoder.set_property("tune", "zerolatency")
-        pay.set_property("pt", 96)
+MUXER_OUTPUT_WIDTH = 1920
+MUXER_OUTPUT_HEIGHT = 1080
+MUXER_BATCH_TIMEOUT_USEC = 4000000
+TILED_OUTPUT_WIDTH = 1280
+TILED_OUTPUT_HEIGHT = 720
+GST_CAPS_FEATURES_NVMM = "memory:NVMM"
 
-        # Connect the "pad-added" signal for the RTSP source
-        source.connect("pad-added", self.on_pad_added, depay)
 
-        # Create the pipeline and add all elements
-        pipeline = Gst.Pipeline.new("rtsp-pipeline")
-        pipeline.add(source)
-        pipeline.add(depay)
-        pipeline.add(decoder)
-        pipeline.add(videobalance)
-        pipeline.add(encoder)
-        pipeline.add(pay)
+def cb_newpad(decodebin, decoder_src_pad, data):
+    print("In cb_newpad\n")
+    caps = decoder_src_pad.get_current_caps()
+    gststruct = caps.get_structure(0)
+    gstname = gststruct.get_name()
+    source_bin = data
+    features = caps.get_features(0)
 
-        # Link elements together
-        if not depay.link(decoder):
-            print("Failed to link depay to decoder")
-        if not decoder.link(videobalance):
-            print("Failed to link decoder to videobalance")
-        if not videobalance.link(encoder):
-            print("Failed to link videobalance to encoder")
-        if not encoder.link(pay):
-            print("Failed to link encoder to pay")
+    # Need to check if the pad created by the decodebin is for video and not
+    # audio.
+    print("gstname=", gstname)
+    if gstname.find("video") != -1:
+        # Link the decodebin pad only if decodebin has picked nvidia
+        # decoder plugin nvdec_*. We do this by checking if the pad caps contain
+        # NVMM memory features.
+        print("features=", features)
+        if features.contains("memory:NVMM"):
+            # Get the source bin ghost pad
+            bin_ghost_pad = source_bin.get_static_pad("src")
+            if not bin_ghost_pad.set_target(decoder_src_pad):
+                sys.stderr.write(
+                    "Failed to link decoder src pad to source bin ghost pad\n"
+                )
+        else:
+            sys.stderr.write(
+                " Error: Decodebin did not pick nvidia decoder plugin.\n")
 
-        return pipeline
 
-    def on_pad_added(self, src, new_pad, depay):
-        # Dynamically link the rtspsrc's pad to the depayloader sink pad
-        sink_pad = depay.get_static_pad("sink")
-        if not sink_pad.is_linked():
-            result = new_pad.link(sink_pad)
-            if result != Gst.PadLinkReturn.OK:
-                print("Failed to link pads!")
-            else:
-                print("Pads linked successfully.")
+def decodebin_child_added(child_proxy, Object, name, user_data):
+    print("Decodebin child added:", name, "\n")
+    if name.find("decodebin") != -1:
+        Object.connect("child-added", decodebin_child_added, user_data)
 
-class Server:
-    def __init__(self, input_uri, output_port):
-        # Create the RTSP server
-        self.server = GstRtspServer.RTSPServer()
-        self.server.set_service(str(output_port))
-        
-        # Set up the relay factory
-        relay_factory = RTSPRelayServer(input_uri)
-        self.server.attach(GObject.MainContext.default())
-        mount_points.add_factory("/relay", relay_factory)
 
-        # Attach the server
-        self.server.attach(None)
-        print(f"RTSP server is streaming at rtsp://127.0.0.1:{output_port}/relay")
+def create_source_bin(index, uri):
+    print("Creating source bin")
 
-if __name__ == "__main__":
-    # Use the provided input RTSP stream URL and desired output port
-    input_uri = "rtsp://EdgeCam:EdgeCam@192.168.2.11:554/stream1"
-    output_port = 8554
+    # Create a source GstBin to abstract this bin's content from the rest of the
+    # pipeline
+    bin_name = "source-bin-%02d" % index
+    print(bin_name)
+    nbin = Gst.Bin.new(bin_name)
+    if not nbin:
+        sys.stderr.write(" Unable to create source bin \n")
 
-    # Start main loop
-    server = Server(input_uri, output_port)
+    # Source element for reading from the uri.
+    # We will use decodebin and let it figure out the container format of the
+    # stream and the codec and plug the appropriate demux and decode plugins.
+    uri_decode_bin = Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+    if not uri_decode_bin:
+        sys.stderr.write(" Unable to create uri decode bin \n")
+    # We set the input uri to the source element
+    uri_decode_bin.set_property("uri", uri)
+    # Connect to the "pad-added" signal of the decodebin which generates a
+    # callback once a new pad for raw data has been created by the decodebin
+    uri_decode_bin.connect("pad-added", cb_newpad, nbin)
+    uri_decode_bin.connect("child-added", decodebin_child_added, nbin)
+
+    # We need to create a ghost pad for the source bin which will act as a proxy
+    # for the video decoder src pad. The ghost pad will not have a target right
+    # now. Once the decode bin creates the video decoder and generates the
+    # cb_newpad callback, we will set the ghost pad target to the video decoder
+    # src pad.
+    Gst.Bin.add(nbin, uri_decode_bin)
+    bin_pad = nbin.add_pad(
+        Gst.GhostPad.new_no_target(
+            "src", Gst.PadDirection.SRC))
+    if not bin_pad:
+        sys.stderr.write(" Failed to add ghost pad in source bin \n")
+        return None
+    return nbin
+
+
+def main(args):
+    # Check input arguments
+    for i in range(0, len(args)):
+        fps_streams["stream{0}".format(i)] = GETFPS(i)
+    number_sources = len(args)
+
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
+
+    # Create gstreamer elements */
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    pipeline = Gst.Pipeline()
+    is_live = False
+
+    if not pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+    print("Creating streamux \n ")
+
+    # Create nvstreammux instance to form batches from one or more sources.
+    streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+    if not streammux:
+        sys.stderr.write(" Unable to create NvStreamMux \n")
+
+    pipeline.add(streammux)
+    for i in range(number_sources):
+        print("Creating source_bin ", i, " \n ")
+        uri_name = args[i]
+        if uri_name.find("rtsp://") == 0:
+            is_live = True
+        source_bin = create_source_bin(i, uri_name)
+        if not source_bin:
+            sys.stderr.write("Unable to create source bin \n")
+        pipeline.add(source_bin)
+        padname = "sink_%u" % i
+        sinkpad = streammux.get_request_pad(padname)
+        if not sinkpad:
+            sys.stderr.write("Unable to create sink pad bin \n")
+        srcpad = source_bin.get_static_pad("src")
+        if not srcpad:
+            sys.stderr.write("Unable to create src pad bin \n")
+        srcpad.link(sinkpad)
+
+    print("Creating tiler \n ")
+    tiler = Gst.ElementFactory.make("nvmultistreamtiler", "nvtiler")
+    if not tiler:
+        sys.stderr.write(" Unable to create tiler \n")
+    print("Creating nvvidconv \n ")
+    nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
+    if not nvvidconv:
+        sys.stderr.write(" Unable to create nvvidconv \n")
+
+    nvvidconv_postosd = Gst.ElementFactory.make(
+        "nvvideoconvert", "convertor_postosd")
+    if not nvvidconv_postosd:
+        sys.stderr.write(" Unable to create nvvidconv_postosd \n")
+
+    # Create a caps filter
+    caps = Gst.ElementFactory.make("capsfilter", "filter")
+    caps.set_property(
+        "caps", Gst.Caps.from_string("video/x-raw(memory:NVMM), format=I420")
+    )
+
+    # Make the encoder
+    if codec == "H264":
+        encoder = Gst.ElementFactory.make("nvv4l2h264enc", "encoder")
+        print("Creating H264 Encoder")
+    elif codec == "H265":
+        encoder = Gst.ElementFactory.make("nvv4l2h265enc", "encoder")
+        print("Creating H265 Encoder")
+    if not encoder:
+        sys.stderr.write(" Unable to create encoder")
+    encoder.set_property("bitrate", bitrate)
+    if is_aarch64():
+        encoder.set_property("preset-level", 1)
+        encoder.set_property("insert-sps-pps", 1)
+        encoder.set_property("bufapi-version", 1)
+
+    # Make the payload-encode video into RTP packets
+    if codec == "H264":
+        rtppay = Gst.ElementFactory.make("rtph264pay", "rtppay")
+        print("Creating H264 rtppay")
+    elif codec == "H265":
+        rtppay = Gst.ElementFactory.make("rtph265pay", "rtppay")
+        print("Creating H265 rtppay")
+    if not rtppay:
+        sys.stderr.write(" Unable to create rtppay")
+
+    # Make the UDP sink
+    updsink_port_num = 5400
+    sink = Gst.ElementFactory.make("udpsink", "udpsink")
+    if not sink:
+        sys.stderr.write(" Unable to create udpsink")
+
+    sink.set_property("host", "224.224.255.255")
+    sink.set_property("port", updsink_port_num)
+    sink.set_property("async", False)
+    sink.set_property("sync", 1)
+
+    streammux.set_property("width", 1920)
+    streammux.set_property("height", 1080)
+    streammux.set_property("batch-size", 1)
+    streammux.set_property("batched-push-timeout", 4000000)
+
+    print("Adding elements to Pipeline \n")
+    tiler_rows = int(math.sqrt(number_sources))
+    tiler_columns = int(math.ceil((1.0 * number_sources) / tiler_rows))
+    tiler.set_property("rows", tiler_rows)
+    tiler.set_property("columns", tiler_columns)
+    tiler.set_property("width", TILED_OUTPUT_WIDTH)
+    tiler.set_property("height", TILED_OUTPUT_HEIGHT)
+    sink.set_property("qos", 0)
+
+    pipeline.add(tiler)
+    pipeline.add(nvvidconv)
+    pipeline.add(nvvidconv_postosd)
+    pipeline.add(caps)
+    pipeline.add(encoder)
+    pipeline.add(rtppay)
+    pipeline.add(sink)
+
+    streammux.link(nvvidconv)
+    nvvidconv.link(tiler)
+    tiler.link(nvvidconv_postosd)
+    nvvidconv_postosd.link(caps)
+    caps.link(encoder)
+    encoder.link(rtppay)
+    rtppay.link(sink)
+
+    # create an event loop and feed gstreamer bus mesages to it
     loop = GObject.MainLoop()
-    loop.run()
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect("message", bus_call, loop)
+
+    # Start streaming
+    rtsp_port_num = 8554
+
+    server = GstRtspServer.RTSPServer.new()
+    server.props.service = "%d" % rtsp_port_num
+    server.attach(None)
+
+    factory = GstRtspServer.RTSPMediaFactory.new()
+    factory.set_launch(
+        '( udpsrc name=pay0 port=%d buffer-size=524288 caps="application/x-rtp, media=video, clock-rate=90000, encoding-name=(string)%s, payload=96 " )'
+        % (updsink_port_num, codec)
+    )
+    factory.set_shared(True)
+    server.get_mount_points().add_factory("/ds-test", factory)
+
+    print(
+        "\n *** DeepStream: Launched RTSP Streaming at rtsp://localhost:%d/ds-test ***\n\n"
+        % rtsp_port_num
+    )
+
+    # start play back and listen to events
+    print("Starting pipeline \n")
+    pipeline.set_state(Gst.State.PLAYING)
+    try:
+        loop.run()
+    except BaseException:
+        pass
+    # cleanup
+    pipeline.set_state(Gst.State.NULL)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='RTSP Output Sample Application Help ')
+    parser.add_argument("-i", "--input",
+                  help="Path to input H264 elementry stream", nargs="+", default=["a"], required=True)
+    parser.add_argument("-c", "--codec", default="H264",
+                  help="RTSP Streaming Codec H264/H265 , default=H264", choices=['H264','H265'])
+    parser.add_argument("-b", "--bitrate", default=4000000,
+                  help="Set the encoding bitrate ", type=int)
+    # Check input arguments
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    global codec
+    global bitrate
+    global stream_path
+    codec = args.codec
+    bitrate = args.bitrate
+    stream_path = args.input
+    return stream_path
+
+if __name__ == '__main__':
+    stream_path = parse_args()
+    sys.exit(main(stream_path))
